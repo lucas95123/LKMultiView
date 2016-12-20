ef5f8255436fb676a495a3a3f9a44dbb0651ce4e
lucas95123 <lucas95123@outlook.com>
2016-12-20 20:19:43 +0800

    implemented MRF

--- LKMultiView/Reconstruct.cpp
+++ LKMultiView/Reconstruct.cpp
@@ -3,6 +3,9 @@
 #include "Reconstruct.h"
 #include "Util.h"
 
+#define LABEL_UNKNOWN 0
+#define TAU1 60
+#define TAU2 150
 
 /**@brief project points in the reference frame to other frame with specified depth
 @param Kref intrinsic matrix of the reference camera
@@ -16,12 +19,12 @@ void projectPointWithDepth(
 	const Matx33d &K,
 	const Mat&rmatRef,
 	const Mat&tmatRef,
-	Point2f &pt, 
-	const Mat &rmat, 
-	const Mat &tmat, 
-	float lower, 
-	float upper, 
-	float step, 
+	Point2f &pt,
+	const Mat &rmat,
+	const Mat &tmat,
+	float lower,
+	float upper,
+	float step,
 	Mat &matPtR)
 {
 	int count = (upper - lower) / step;
@@ -34,7 +37,7 @@ void projectPointWithDepth(
 	Vec3f vecPt3D = K.inv()*Vec3f(pt.x, pt.y, 1);
 
 	//projection matrix's inverse from rotation and translation
-	Mat projRefInv=Mat::zeros(4, 4, CV_32FC1);
+	Mat projRefInv = Mat::zeros(4, 4, CV_32FC1);
 	Mat tmpRotation = rmatRef.t();
 	Mat tmpTranslation = -rmatRef.t()*tmatRef;
 	tmpRotation.convertTo(projRefInv(Range(0, 3), Range(0, 3)), CV_32FC1);
@@ -52,9 +55,9 @@ void projectPointWithDepth(
 		vecCoord(2) = vecPt3D(2)*depth;
 		vecCoord(3) = 1.f;
 		res = projRefInv*Mat(vecCoord);
-		matPt.at<float>(i,0) = res.at<float>(0);
-		matPt.at<float>(i,1) = res.at<float>(1);
-		matPt.at<float>(i,2) = res.at<float>(2);
+		matPt.at<float>(i, 0) = res.at<float>(0);
+		matPt.at<float>(i, 1) = res.at<float>(1);
+		matPt.at<float>(i, 2) = res.at<float>(2);
 	}
 
 	//convert from rotation matrix to vector
@@ -74,14 +77,45 @@ void projectPointWithDepth(
 @param the upper depth bound
 @param the depth step
 */
+void calcDepthFromPCResponseWithMRF(
+	const Mat& matPC,
+	GCoptimizationGridGraph *gc,
+	int pixelIndex,
+	Metric metric,
+	float dL,
+	float dU,
+	float dS,
+	float threshold,
+	bool visualize = false)
+{
+	//finding the average of PC Response
+	Mat matAvg = Mat(1, matPC.cols, CV_32FC1);
+	for (int col = 0; col < matPC.cols; col++)
+	{
+		float sum = 0.0;
+		for (int row = 0; row < matPC.rows; row++)
+			sum += matPC.at<float>(row, col);
+		matAvg.at<float>(col) = sum / matPC.rows;
+	}
+	for (int i = 1; i < matAvg.cols + 1; i++)
+		gc->setDataCost(pixelIndex, i, (int)(100 - matAvg.at<float>(i) * 100));
+	gc->setDataCost(pixelIndex, LABEL_UNKNOWN, TAU1);
+}
+
+/**@calculate the index of the most likely depth
+@param the Photo-Consistency Response matrix
+@param the lower depth bound
+@param the upper depth bound
+@param the depth step
+*/
 float calcDepthFromPCResponse(
-	const Mat& matPC, 
-	Metric metric, 
-	float dL, 
-	float dU, 
-	float dS, 
-	float threshold, 
-	bool visualize=false)
+	const Mat& matPC,
+	Metric metric,
+	float dL,
+	float dU,
+	float dS,
+	float threshold,
+	bool visualize = false)
 {
 	//finding the average of PC Response
 	Mat matAvg = Mat(1, matPC.cols, CV_32FC1);
@@ -104,57 +138,7 @@ float calcDepthFromPCResponse(
 		}
 	}
 
-	if(visualize)
-		visualizePCScores(matPC, matAvg, 500);
-
-	if (dMax < 0.7)
-		return 0;
-	return	dL + iMax*dS;
-}
-
-/**@calculate the index of the most likely depth using robust NCC
-@param the Photo-Consistency Response matrix
-@param the lower depth bound
-@param the upper depth bound
-@param the depth step
-*/
-float calcDepthFromPCResponseRobust(
-	const Mat& matPC, 
-	Metric metric, 
-	float dL, 
-	float dU, 
-	float dS, 
-	float threshold,
-	bool visualize=false)
-{
-	//finding the average of PC Response
-	Mat matAvg = Mat(1, matPC.cols, CV_32FC1);
-	float ftmp = 0.f;
-	for (int col = 0; col < matPC.cols; col++)
-	{
-		float sum = 0.0;
-		for (int row = 0; row < matPC.rows; row++)
-		{
-			ftmp = matPC.at<float>(row, col);
-			sum += ftmp < 0 ? 0 : ftmp;
-		}
-		matAvg.at<float>(col) = sum / matPC.rows;
-	}
-
-	//finding global maxima of the averaged curve
-	int iMax = 0;
-	float dMax = 0.0;
-	dMax = matAvg.at<float>(0);
-	for (int i = 0; i < matAvg.cols; i++)
-	{
-		if (matAvg.at<float>(i) > dMax)
-		{
-			dMax = matAvg.at<float>(i);
-			iMax = i;
-		}
-	}
-
-	if(visualize)
+	if (visualize)
 		visualizePCScores(matPC, matAvg, 500);
 
 	if (dMax < threshold)
@@ -168,28 +152,88 @@ float calcDepthFromPCResponseRobust(
 @param the upper depth bound
 @param the depth step
 */
-float calcDepthFromPCResponseParzen(
-	const Mat& matPC, 
-	Metric metric, 
-	float dL, 
-	float dU, 
+void calcDepthFromPCResponseParzenWithMRF(
+	const Mat& matPC,
+	GCoptimizationGridGraph *gc,
+	int pixelIndex,
+	Metric metric,
+	float dL,
+	float dU,
 	float dS,
-	float threshold,
 	int kernelSize,
 	bool visualize)
 {
 	//finds local maximas
-	Mat matLocalMaxima=Mat::zeros(matPC.rows, matPC.cols, CV_32FC1);
+	Mat matLocalMaxima = Mat::zeros(matPC.rows, matPC.cols, CV_32FC1);
 	for (int i = 0; i < matPC.rows; i++)
 	{
-		for (int j = 1; j < matPC.cols-1; j++)
+		for (int j = 1; j < matPC.cols - 1; j++)
 		{
 			if (matPC.at<float>(i, j) > matPC.at<float>(i, j - 1) && matPC.at<float>(i, j) > matPC.at<float>(i, j + 1))
 				matLocalMaxima.at<float>(i, j) = matPC.at<float>(i, j);
 		}
 	}
 
-	Mat matResult=Mat::zeros(1, matPC.cols, CV_32FC1);
+	Mat matResult = Mat::zeros(1, matPC.cols, CV_32FC1);
+	//using parzen window
+	GaussianKernel kernel = GaussianKernel(3);
+	for (int i = 0; i < matLocalMaxima.cols; i++)
+	{
+		float sum = 0.f;
+		int count = 0.f;
+		for (int j = i - kernelSize / 2; j < i + kernelSize / 2; j++)
+		{
+			if (j < 0 || j >= matLocalMaxima.cols)
+			{
+				sum += 0;
+				continue;
+			}
+			for (int k = 0; k < matLocalMaxima.rows; k++)
+			{
+				float tmp = matLocalMaxima.at<float>(k, j);
+				if (tmp == 0.f)
+					continue;
+				else
+				{
+					sum += tmp*kernel.getVal(j - i);
+					count++;
+				}
+			}
+		}
+		matResult.at<float>(i) = sum;
+		gc->setDataCost(pixelIndex, i, min(100, (int)(100 - sum * 100)));
+	}
+	gc->setDataCost(pixelIndex, LABEL_UNKNOWN, 600);
+}
+
+/**@calculate the index of the most likely depth using parzen window method
+@param the Photo-Consistency Response matrix
+@param the lower depth bound
+@param the upper depth bound
+@param the depth step
+*/
+float calcDepthFromPCResponseParzen(
+	const Mat& matPC,
+	Metric metric,
+	float dL,
+	float dU,
+	float dS,
+	float threshold,
+	int kernelSize,
+	bool visualize)
+{
+	//finds local maximas
+	Mat matLocalMaxima = Mat::zeros(matPC.rows, matPC.cols, CV_32FC1);
+	for (int i = 0; i < matPC.rows; i++)
+	{
+		for (int j = 1; j < matPC.cols - 1; j++)
+		{
+			if (matPC.at<float>(i, j) > matPC.at<float>(i, j - 1) && matPC.at<float>(i, j) > matPC.at<float>(i, j + 1))
+				matLocalMaxima.at<float>(i, j) = matPC.at<float>(i, j);
+		}
+	}
+
+	Mat matResult = Mat::zeros(1, matPC.cols, CV_32FC1);
 	//using parzen window
 	GaussianKernel kernel = GaussianKernel(3);
 	for (int i = kernelSize / 2; i < matLocalMaxima.cols - kernelSize / 2; i++)
@@ -205,12 +249,12 @@ float calcDepthFromPCResponseParzen(
 					continue;
 				else
 				{
-					sum += tmp*kernel.getVal(j-i);
+					sum += tmp*kernel.getVal(j - i);
 					count++;
 				}
 			}
 		}
-		matResult.at<float>(0,i) = sum;
+		matResult.at<float>(0, i) = sum;
 	}
 
 	//finding global maxima of the averaged curve
@@ -226,9 +270,150 @@ float calcDepthFromPCResponseParzen(
 		}
 	}
 
-	if (dMax < 0.6)
+	if (dMax < threshold)
 		return 0;
 	return	dL + iMax*dS;
+}
+
+/**@brief calculate the depth map from multiple images and their relative position from reference image
+@param matDepth the output depth map
+@param vecImageNames the input list of image names
+@param K the camera intrinsic matrix
+@param vecRotation list of rotations obtained by trackCamera()
+@param vecTranslation list of translations obtained by trackCamera()
+@param method for Photo-Consistency metric, Reconstruct::NCC etc
+@param patchSize size of the matching patch
+@param lower min value of searching depth
+@param upper max value of searching depth
+@param step step of searching depth
+*/
+void calcDepthMapFromMultiViewWithMRF(
+	Mat &matDepth,
+	vector<string> &vecImgNames,
+	Mat &K,
+	vector<Mat> &vecRotation,
+	vector<Mat> &vecTranslation,
+	Reconstruct method,
+	Metric metric,
+	int patchSize,
+	float lower,
+	float upper,
+	float step)
+{
+	//vector storing images
+	vector<Mat> vecImg;
+	for (int i = 0; i < vecImgNames.size(); i++)
+		vecImg.push_back(imread(vecImgNames[i]));
+
+	//set the first image as the reference image
+	Mat matImgRef = vecImg[0];
+
+	//initialize the depth map
+	matDepth = Mat::zeros(matImgRef.rows, matImgRef.cols, CV_32FC1);
+
+	int iWidth = matImgRef.cols;
+	int iHeight = matImgRef.rows;
+	int iSteps = (upper - lower) / step;
+
+	//sliding window patches
+	Patch patchL(patchSize);
+	Patch patchR(patchSize);
+
+	//GCoptimization, one more state for unknown label
+	GCoptimizationGridGraph *gc = new GCoptimizationGridGraph(iWidth, iHeight, iSteps + 1);
+
+	for (int row = 0; row < iHeight; row++)
+	{
+		for (int col = 0; col < iWidth; col++)
+		{
+			//for borders force to be unknown label penalize regular label
+			if (row < patchSize / 2 || row >= iHeight - patchSize / 2 || col < patchSize / 2 || col >= iWidth - patchSize / 2)
+			{
+				for (int l = 1; l < iSteps + 1; l++)
+					gc->setDataCost(row*iWidth + col, l, 100);
+				gc->setDataCost(row*iWidth + col, LABEL_UNKNOWN, 0);
+				continue;
+			}
+			//mat storing Photo-Consistency response of different depth in different frame column is the frame row is the response to different depth
+			Mat matPCScore = Mat::zeros(vecRotation.size() - 1, iSteps, CV_32FC1);
+
+			//current point in the reference image
+			Point2f ptL(col, row);
+
+			//point in the other image
+			Point2f ptR;
+
+			//for every possible depth in depth range
+			for (int iImgIdx = 1; iImgIdx < vecRotation.size(); iImgIdx++)
+			{
+				//project 3d point to other image with every possible depth
+				Mat matPtR;
+				float dPCScore = 0.0;
+
+				projectPointWithDepth(
+					K,
+					vecRotation[0],
+					vecTranslation[0],
+					ptL,
+					vecRotation[iImgIdx],
+					vecTranslation[iImgIdx],
+					lower,
+					upper,
+					step,
+					matPtR
+				);
+
+				for (int i = 0; i < iSteps; i++)
+				{
+					//get patches from reference image and other image
+					ptR.x = matPtR.at<float>(i, 0);
+					ptR.y = matPtR.at<float>(i, 1);
+					if (ptR.x >= patchSize / 2 && ptR.x < iWidth - patchSize&&ptR.y >= patchSize / 2 && ptR.y < iHeight - patchSize / 2)
+					{
+						getPatchFromImage(matImgRef, ptL, patchL);
+						getPatchFromImage(vecImg[iImgIdx], ptR, patchR);
+						//calculate photo-consistency metric
+						dPCScore = Patch::calcNCC(patchL, patchR, metric);
+					}
+					else
+						dPCScore = 0;
+					matPCScore.at<float>(iImgIdx - 1, i) = dPCScore;
+				}
+			}
+			calcDepthFromPCResponseWithMRF(
+				matPCScore,
+				gc,
+				row*iWidth + col,
+				metric,
+				lower,
+				upper,
+				step,
+				3,
+				false
+			);
+		}
+		cout << "Complete Percent: " << ((float)row) / iHeight * 100 << "%              " << "\r";
+	}
+
+	for (int l1 = 0; l1 < iSteps + 1; l1++)
+		for (int l2 = 0; l2 < iSteps + 1; l2++)
+		{
+			if (l1 == LABEL_UNKNOWN && l2 == LABEL_UNKNOWN)
+				gc->setSmoothCost(l1, l2, 0);
+			else if (l1 == LABEL_UNKNOWN && l2 != LABEL_UNKNOWN || l1 != LABEL_UNKNOWN && l2 == LABEL_UNKNOWN)
+				gc->setSmoothCost(l1, l2, TAU2);
+			else
+				gc->setSmoothCost(l1, l2, abs(l1 - l2));
+		}
+
+	cout << "Before optimization: " << gc->compute_energy() << endl;
+	gc->expansion(10);
+	cout << "After optimization: " << gc->compute_energy() << endl;
+
+	for (int i = 0; i < iHeight*iWidth; i++)
+		matDepth.at<float>(i) = gc->whatLabel(i) == 0 ? 0 : (gc->whatLabel(i)-1)*step + lower;
+
+	delete gc;
 }
 
 /**@brief calculate the depth map from multiple images and their relative position from reference image
@@ -276,7 +461,7 @@ void calcDepthMapFromMultiView(
 	//Mat matL = vecImg[0].clone();
 	//Mat matR = vecImg[testIndex].clone();
 
-	//Point2f ptTest = Point2f(397, 329);
+	//Point2f ptTest = Point2f(200, 229);
 	//projectPointWithDepth(
 	//	K,
 	//	vecRotation[0],
@@ -321,7 +506,7 @@ void calcDepthMapFromMultiView(
 		{
 
 			//mat storing Photo-Consistency response of different depth in different frame column is the frame row is the response to different depth
-			Mat matPCScore = Mat::zeros(vecRotation.size()-1, iSteps, CV_32FC1);
+			Mat matPCScore = Mat::zeros(vecRotation.size() - 1, iSteps, CV_32FC1);
 
 			//current point in the reference image
 			Point2f ptL(col, row);
@@ -362,7 +547,7 @@ void calcDepthMapFromMultiView(
 					else
 						dPCScore = 0;
 					matPCScore.at<float>(iImgIdx - 1, i) = dPCScore;
-				}	
+				}
 			}
 			float dRes = calcDepthFromPCResponse(matPCScore, metric, lower, upper, step, 0.5, false);
 			//cout << dRes << endl;
--- LKMultiView/Reconstruct.h
+++ LKMultiView/Reconstruct.h
@@ -25,3 +25,16 @@ void calcDepthMapFromMultiView(
 	float lower = 1.0,
 	float upper = 11.0,
 	float step = 1);
+
+void calcDepthMapFromMultiViewWithMRF(
+	Mat &matDepth,
+	vector<string> &vecImgNames,
+	Mat &K,
+	vector<Mat> &vecRotation,
+	vector<Mat> &vecTranslation,
+	Reconstruct method,
+	Metric metric,
+	int patchSize = 5,
+	float lower = 1.0,
+	float upper = 11.0,
+	float step = 1);
